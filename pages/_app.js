8b5ae99967cd429fd358c4a4983824813add5300
bolub <abiol5202@gmail.com>
2022-02-28 00:11:15 +0100

    Update school fees logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/global.css';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import GradCap from '../Components/UI/GradCap';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -18,8 +18,20 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <div className='border-8 min-h-screen border-blue-500 flex flex-col'>
-        <div className='mx-auto md:mx-0 md:pl-10 pt-10'>
-          <img className='w-48' src='/logo.png' />
+        <div className='md:mx-0 pl-8 md:pl-16 pt-16 flex flex-col md:flex-row justify-center'>
+          <div className='w-48 my-auto'>
+            <img src='/logo.png' />
+          </div>
+
+          <div className='flex mt-4 md:my-auto md:ml-4'>
+            <div className='bg-blue-500 w-14 h-14 p-1 flex rounded-full my-auto'>
+              <GradCap className={'text-white w-8 h-8 m-auto'} />
+            </div>
+
+            <p className='my-auto font-semibold text-blue-500 uppercase ml-2 text-md'>
+              Paylater School Fees
+            </p>
+          </div>
         </div>
         <Component {...pageProps} />
       </div>
